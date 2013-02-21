a05a06434a636699e00d3d3737557b2f3d01473a
alice.grange05@gmail.com <alice.grange05@gmail.com@a4b46564-11a1-6cd7-8d02-baac891978e0>
2013-02-21 20:20:39 +0000

    reorganisation / attributs / methodes creation classe test

--- ArbreHuffman.java
+++ ArbreHuffman.java
@@ -6,46 +6,49 @@ import codagehuffman.Noeud;
 public class ArbreHuffman<E extends Comparable<E>> extends ArbreBinaire<E> {
 
 	// Un arbre de huffman est un type d'arbre binaire mais a en plus une
-	// priorit�
+	// priorite
 
 	private int priorite;
 
 	/** Cree un arbre de huffman vide de priorit� 0 */
 	public ArbreHuffman() {
-            this.racine = null;
-            this.priorite = 0;
+		this.racine = null;
+		this.priorite = 0;
 	}
 
-	/** Cree un arbre de huffman contenant une unique valeur de priorit� 1 */
+	/** Cree un arbre de huffman contenant une unique valeur de priorite 1 */
 	public ArbreHuffman(E val) {
-            Noeud<E> racine = new Noeud<E>(val);
-            this.racine = racine;
-            this.priorite = 1;
+		Noeud<E> racine = new Noeud<E>(val);
+		this.racine = racine;
+		this.priorite = 1;
 	}
 
-	/** Cree un arbre de huffman contenant une unique valeur de priorit� donn�e */
+	/**
+	 * Cree un arbre de huffman contenant une unique valeur de priorite
+	 * donnee
+	 */
 	public ArbreHuffman(E val, int priorite) {
-            Noeud<E> racine = new Noeud<E>(val);
-            this.racine = racine;
-            this.priorite = priorite;
-	}
-        
-        public ArbreHuffman(Noeud<E> racine, int priorite) {
-            this.racine = racine;
-            this.priorite = priorite;
+		Noeud<E> racine = new Noeud<E>(val);
+		this.racine = racine;
+		this.priorite = priorite;
 	}
 
-	/** Renvoie la priorit� de l'arbre de huffman */
+	public ArbreHuffman(Noeud<E> racine, int priorite) {
+		this.racine = racine;
+		this.priorite = priorite;
+	}
+
+	/** Renvoie la priorite de l'arbre de huffman */
 	public int getPriorite() {
-            return this.priorite;
+		return this.priorite;
 	}
 
-	/** Incr�mente de un la priorit� de l'arbre de huffman */
+	/** Incremente de un la priorite de l'arbre de huffman */
 	public void incrementerPriorite() {
-            this.priorite++;
+		this.priorite++;
 	}
-        
-        public int nbNoeuds() {
+
+	public int nbNoeuds() {
 		if (estVide())
 			return 0;
 
