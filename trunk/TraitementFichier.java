b44499c8988af0053d278501af7c040cc9baad16
r.lhortolat@gmail.com <r.lhortolat@gmail.com@a4b46564-11a1-6cd7-8d02-baac891978e0>
2013-02-21 16:57:41 +0000

    coucou !

--- trunk/TraitementFichier.java
+++ trunk/TraitementFichier.java
@@ -1,18 +1,24 @@
 package codagehuffman;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Hashtable;
+import java.util.Iterator;
 
-abstract class TraitementFichier {
+public class TraitementFichier {
+    
+        protected Hashtable<Integer, ArbreHuffman<Character>> arbreHuffmanLettresFichier;
 
 	/** Lit les caracteres du fichier a compresser
 	 *  Ajoute chaque caractere dans la Hashtable si non pr�sent
 	 *  Sinon modifie leur priorite*/
-            public static Hashtable<Integer, ArbreHuffman<Character>> lire(
+            public void lireDecompresse(
 			String nomFichier) {
 		try {
 			BufferedReader fichierTexte = new BufferedReader(new FileReader(
@@ -25,6 +31,7 @@ abstract class TraitementFichier {
 						+ " n'a pas ete trouve:");
 
 			while ((caractereLuCodeAscii = fichierTexte.read()) != -1) {
+                            //this.texte += (char)caractereLuCodeAscii;
 				if (!arbreHuffmanLettresFichier
 						.containsKey(caractereLuCodeAscii)) {
 					arbreHuffmanLettresFichier.put(caractereLuCodeAscii,
@@ -36,21 +43,70 @@ abstract class TraitementFichier {
 				}
 			}
 
-			return arbreHuffmanLettresFichier;
-
+                        this.arbreHuffmanLettresFichier = arbreHuffmanLettresFichier;
 		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
-
-			return null;
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
-
-			return null;
 		}
 
 	}
+            
+        public void lireCompresse(String nomFichier){
+            
+        }
 
-	public void ecrire() {
+	public void ecrireCompresse(String entete) { // A revoir
+            File f = new File ("C:/Users/Romain/Documents/NetBeansProjects/CodageHuffman/src/codagehuffman/fichierTexteCompresse.txt");
+             DataOutputStream  dos = null;
+             String chaineAecrire = "";
+             int caractereLuCodeAscii;
+            
+            try{
+                dos = new DataOutputStream (new FileOutputStream (f));
+            }catch(IOException e){
+                System.out.println(e.getMessage());
+            }
+            
+            String[] enteteElements = entete.split("\\|");
+            Hashtable<String, String> ht = new Hashtable();
+            
+            for(int i=0; i<enteteElements.length; i=i+3){
+                ht.put(enteteElements[i+1], enteteElements[i]);
+            }
+            
+            try{
+                BufferedReader fichierTexte = new BufferedReader(new FileReader(
+                                            new File("C:/Users/Romain/Documents/NetBeansProjects/CodageHuffman/src/codagehuffman/fichierTexte.txt"))); // A changer pour le nom du fichier
 
+                while ((caractereLuCodeAscii = fichierTexte.read()) != -1) {
+                    chaineAecrire += ht.get((char)caractereLuCodeAscii);
+                }
+            }catch(IOException e){
+                //blabla
+            }
+            
+            chaineAecrire = entete+chaineAecrire;
+            
+            try{   
+                int conv;
+                for(int i=0;i<chaineAecrire.length();i+=8){
+                    conv = 0;
+                    //convertit 8 bits :
+                    for(int j=0;j<8;j++){
+                        conv = conv<<1;   //décale d'un bit vers la gauche ton nombre convertit (revient a multiplier par 2!)
+
+                         //conversion du bit
+                         conv = conv+(chaineAecrire.charAt(i+j)-'0');
+                     }
+
+                     //ecriture dans le fichier :
+                     dos.write(conv);
+                }
+                
+                dos.close();
+            }catch(IOException e){
+                System.out.println(e.getMessage());
+            }
 	}
 }
