a002cdd3c95d0dafd77235bb85deda39cfa40299
alice.grange05@gmail.com <alice.grange05@gmail.com@a4b46564-11a1-6cd7-8d02-baac891978e0>
2013-03-01 10:35:16 +0000

    modif test codage

--- trunk/TestCodageHuffman.java
+++ trunk/TestCodageHuffman.java
@@ -5,19 +5,54 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class TestCodageHuffman {
+	
+	//Variables pour gerer les fichiers
+	static String nomFichierOriginal = "C:/Users/Lutine/Documents/ExemplesTexteAlgo/fichierTexte1.txt";
+	static String nomFichierCompresse = "C:/Users/Lutine/Documents/ExemplesTexteAlgo/fichierTexteCompresse1.txt";
+	static String nomFichierDecompresse = "C:/Users/Lutine/Documents/ExemplesTexteAlgo/fichierTexteDecompresse1.txt";
+	
+	 /*
+	 * static String nomFichierCompresse =
+	 * "C:/Users/Romain/Documents/NetBeansProjects/CodageHuffman/src/codagehuffman/fichierTexteCompresse.txt"
+	 * ; 
+	 * static String nomFichierDecompresse =
+	 * "C:/Users/Romain/Documents/NetBeansProjects/CodageHuffman/src/codagehuffman/fichierTexteDecompresse.txt"
+	 * ;
+	 */
+	
+	// Variables pour les temps d'exécution
+	static long start;
+	static long duree;
+	
+	public static void main(String[] args) throws FileNotFoundException,
+			IOException {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-		//String nomFichierDecompresse = "C:/Users/Lutine/Documents/fichierTexte1.txt";
-		//String nomFichierModif1 = "C:/Users/Lutine/Documents/fichierTexteModif1.txt";
-                String nomFichierOriginal = "C:/Users/Romain/Documents/NetBeansProjects/CodageHuffman/src/codagehuffman/fichierTexte.txt";
-                String nomFichierCompresse = "C:/Users/Romain/Documents/NetBeansProjects/CodageHuffman/src/codagehuffman/fichierTexteCompresse.txt";
-                String nomFichierDecompresse = "C:/Users/Romain/Documents/NetBeansProjects/CodageHuffman/src/codagehuffman/fichierTexteDecompresse.txt";
-                
-		CodageHuffman c1 = new CodageHuffman(nomFichierOriginal,nomFichierCompresse);
-		//c1.compresserFichier();
-                
-                CodageHuffman c2 = new CodageHuffman(nomFichierCompresse,nomFichierDecompresse);
-                c2.decompresserFichier();
+		System.out.println("_____________________FICHIER TEXTE 1_____________________");
+		start = System.nanoTime();
+		testCompression();
+		duree = System.nanoTime() - start;	
+		System.out.println("Duree d'éxécution pour la compression du fichier: "+duree+" ns");
+		
+		start = System.nanoTime();
+		testDecompression();	
+		duree = System.nanoTime() - start;
+		System.out.println("Duree d'éxécution pour la décompression du fichier: "+duree+" ns");		
 
+	}
+	
+	private static void testCompression() throws FileNotFoundException, IOException{
+		System.out.println("********************COMPRESSION FICHIER*******************");
+		CodageHuffman c1 = new CodageHuffman(nomFichierOriginal,
+				nomFichierCompresse);
+		// Compression du fichier original
+		c1.compresserFichier();	
+	}
+	
+	private static void testDecompression() throws FileNotFoundException, IOException{
+		System.out.println("*******************DECOMPRESSION FICHIER******************");
+		CodageHuffman c2 = new CodageHuffman(nomFichierCompresse,
+				nomFichierDecompresse);
+		// Decompression du fichier que l'on a compressé
+		c2.decompresserFichier();
 	}
 }
